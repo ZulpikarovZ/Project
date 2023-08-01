24e283c76d7c9bf59b0265cb4fc3128f322cbace
Zaur <Zulpikarov.Z@gmail.com>
2023-08-01 19:18:44 +0300

    commit with JSX

---
+++ project-1/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// Вся разметка html имеет декларативный подход как и функция new Date()
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+					<br />
+					{new Date().getFullYear()}
+				</a>
+			</header>
+		</div>
+	);
+};
+
+export default App;
